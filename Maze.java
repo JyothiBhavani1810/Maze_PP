d930a5ab9293c6116695d28b5f997aea574ba75e
JyothiBhavani1810 <129309594+JyothiBhavani1810@users.noreply.github.com>
2023-03-31 14:30:35 +0000

    commit

---
+++ Maze.java
@@ -0,0 +1,11 @@
+public class Maze{
+    private int size;
+    private int[][] nodes;
+    private Graph graph;
+
+    public static void main(String args[])
+    {
+        System.out.println("Welocme to 2D maze!");
+    }
+
+}
